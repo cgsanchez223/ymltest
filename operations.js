4d29102ca6f6806f8dfced8ec3132f8a9c57a4fd
Christopher Sanchez <cgsanchez223@gmail.com>
2024-08-15 15:40:01 -0400

    initial commit

---
+++ operations.js
@@ -0,0 +1,21 @@
+function add(a = 0, b = 0) {
+    return a + b;
+}
+
+function subtract(a = 0, b = 0) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function average(...nums) {
+    if (nums.length === 0) return 0;
+    const total = nums.reduce(function(acc, next) {
+        return add(acc, next);
+    });
+    return total / nums.length;
+}
+
+module.exports = { add, average, multiply, subtract };
---
+++ operations.test.js
@@ -0,0 +1,36 @@
+const { add, average, multiply, subtract } = require("./operations");
+
+describe("#add", function() {
+    it("adds numbers", function() {
+        expect(add(2, 2)).toEqual(4);
+    });
+    it("handles empty inputs", function() {
+        expect(add()).toEqual(0);
+    });
+});
+
+describe("#subtract", function() {
+    it("subtracts numbers", function() {
+        expect(subtract(2, 2)).toEqual(0);
+    });
+    it("handles empty inputs", function() {
+        expect(subtract()).toEqual(0);
+    });
+});
+
+describe("multiply", function() {
+    it("multiplies two numbers", function() {
+        expect(multiply(2, 3)).toEqual(6);
+        expect(multiply(-4, 5)).toEqual(-20);
+    });
+});
+
+describe("#average", function() {
+    it("calculates the average", function() {
+        expect(average(2, 2)).toEqual(2);
+        expect(average(2, -2)).toEqual(0);
+    });
+    it("handles empty inputs", function() {
+        expect(average()).toEqual(0);
+    });
+});
